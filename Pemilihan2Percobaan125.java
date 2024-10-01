f4d38ba91fda5b971eaa8f255c8bf180d342ebf9
wahyudi <wahyudisatriawanh@gmail.com>
2024-10-02 00:53:12 +0700

    Penyelesaian percobaan 1

--- Pemilihan2Percobaan125.java
+++ Pemilihan2Percobaan125.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 public class Pemilihan2Percobaan125 {
-    public static void main(String[] args) {
-            Scanner input25 = new Scanner(System.in);
+public static void main(String[] args) {
+Scanner input25 = new Scanner(System.in);
 
-                    int tahun;
+int tahun;
 
-                            System.out.print("Masukkan Tahun: ");
-                                    tahun = input25.nextInt();
+System.out.print("Masukkan Tahun: ");
+tahun = input25.nextInt();
                                             
-                                                    if ((tahun % 4) == 0) {
-                                                                if ((tahun % 100) != 0)
-                                                                            System.out.println("Tahun Kabisat");
-                                                                                    } else
-                                                                                                System.out.println("Bukan Tahun Kabisat");
-                                                                                                    }
-                                                                                                    }
+if ((tahun % 4) == 0) {
+    if ((tahun % 100) != 0)
+    System.out.println("Tahun Kabisat");
+    } else
+     System.out.println("Bukan Tahun Kabisat");
+          
+    }        
+}
