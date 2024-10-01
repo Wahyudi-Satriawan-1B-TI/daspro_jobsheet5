74ceb5565e63805b84eef5957bac647c32c4de91
wahyudi <wahyudisatriawanh@gmail.com>
2024-10-02 00:49:41 +0700

    penyelesaian percobaan 1

---
+++ Pemilihan2Percobaan125.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan125 {
+    public static void main(String[] args) {
+            Scanner input25 = new Scanner(System.in);
+
+                    int tahun;
+
+                            System.out.print("Masukkan Tahun: ");
+                                    tahun = input25.nextInt();
+                                            
+                                                    if ((tahun % 4) == 0) {
+                                                                if ((tahun % 100) != 0)
+                                                                            System.out.println("Tahun Kabisat");
+                                                                                    } else
+                                                                                                System.out.println("Bukan Tahun Kabisat");
+                                                                                                    }
+                                                                                                    }
