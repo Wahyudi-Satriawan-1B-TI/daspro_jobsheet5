f3a6fec9a770b531f6f28363cd52618f2c72bff2
wahyudi <wahyudisatriawanh@gmail.com>
2024-10-02 01:10:44 +0700

    Penyelesaian Pertanyaan Nomor 5

--- Pemilihan2Percobaan125.java
+++ Pemilihan2Percobaan125.java
@@ -11,6 +11,8 @@ tahun = input25.nextInt();
 if ((tahun % 4) == 0) {
     if ((tahun % 100) != 0) {
     System.out.println("Tahun Kabisat");
+    } else if ((tahun % 400) == 0) {
+    System.out.println("Tahun Kabisat");
     } else
     System.out.println("Bukan Tahun Kabisat");
 } else
